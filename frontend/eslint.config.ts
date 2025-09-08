9d9c199e7bdd43a8d5b217c303046726f9eb4fc5
Zach Preston <zachattack6089@gmail.com>
2025-09-07 19:23:19 -0700

    Formatting changes, Git cleanup.

--- frontend/eslint.config.ts
+++ frontend/eslint.config.ts
@@ -1,6 +1,6 @@
-import { globalIgnores } from 'eslint/config'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
+import { globalIgnores } from 'eslint/config'
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -8,13 +8,19 @@ import pluginVue from 'eslint-plugin-vue'
 // More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
 
 export default defineConfigWithVueTs(
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
-  },
+    {
+        name: 'app/files-to-lint',
+        files: [
+            '**/*.{ts,mts,tsx,vue}'
+        ],
+    },
 
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+    globalIgnores([
+        '**/dist/**',
+        '**/dist-ssr/**',
+        '**/coverage/**'
+    ]),
 
-  pluginVue.configs['flat/essential'],
-  vueTsConfigs.recommended,
+    pluginVue.configs['flat/essential'],
+    vueTsConfigs.recommended,
 )
