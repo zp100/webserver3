e75582da8973737a8ea61b660a34c7f16550fe7d
Zach Preston <zachattack6089@gmail.com>
2025-09-07 19:25:18 -0700

    Copied 'hello world' Vue code.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    import { ref } from 'vue'
+
+    const server_status = ref('...')
+
+    async function get_server_status() {
+        const host = 'http://localhost'
+        const port = 8081
+
+        const status_url = `${host}:${port}/status`
+        const response = await fetch(status_url)
+        const result = await response.json()
+        server_status.value = result['status']
+    }
+</script>
 
 <template>
-    <h1>You did it!</h1>
-    <p>
-        Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-        documentation
-    </p>
+    <h2>Hello world!</h2>
+    <div>Status response from server: {{ server_status }}</div>
+    <button @click="get_server_status">Fetch</button>
 </template>
 
 <style scoped></style>
