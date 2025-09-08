b15d81b00ec5527de050a06b07a9aabfb08616c0
Zach Preston <zachattack6089@gmail.com>
2025-09-07 20:17:32 -0700

    Added error handling for database requests.

--- backend/app.js
+++ backend/app.js
@@ -4,20 +4,27 @@ const mysql = require('mysql2/promise')
 const app = express();
 
 app.get('/status', async (req, res) => {
-    const connection = await mysql.createConnection({
-        host: process.env['DB_HOST'],
-        user: process.env['DB_USER'],
-        password: process.env['DB_PASSWORD'],
-        database: process.env['DB_DATABASE'],
-    });
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env['DB_HOST'],
+            user: process.env['DB_USER'],
+            password: process.env['DB_PASSWORD'],
+            database: process.env['DB_DATABASE'],
+        });
 
-    const [ results, fields ] = await connection.query(`
-        SELECT * FROM Status LIMIT 1
-    `)
+        const [ results, fields ] = await connection.query(`
+            SELECT * FROM Status LIMIT 1
+        `)
 
-    res.send({
-        status: results[0]['status'],
-    })
+        res.send({
+            status: results[0]['status'],
+        })
+    } catch (e) {
+        console.error(e.message)
+        res.send({
+            status: 'Database error...',
+        })
+    }
 })
 
 app.listen(process.env['PORT'] ?? 8081);
