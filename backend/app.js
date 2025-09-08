f585f4be4dbfb0ea0fdc20b6b40c6b4f9190848f
Zach Preston <zachattack6089@gmail.com>
2025-09-07 20:55:25 -0700

    Removed database for now.

--- backend/app.js
+++ backend/app.js
@@ -6,27 +6,31 @@ const app = express()
 app.use(cors())
 
 app.get('/status', async (req, res) => {
-    try {
-        const connection = await mysql.createConnection({
-            host: process.env['DB_HOST'],
-            user: process.env['DB_USER'],
-            password: process.env['DB_PASSWORD'],
-            database: process.env['DB_DATABASE'],
-        });
+    // try {
+    //     const connection = await mysql.createConnection({
+    //         host: process.env['DB_HOST'],
+    //         user: process.env['DB_USER'],
+    //         password: process.env['DB_PASSWORD'],
+    //         database: process.env['DB_DATABASE'],
+    //     });
 
-        const [ results, fields ] = await connection.query(`
-            SELECT * FROM Status LIMIT 1
-        `)
+    //     const [ results, fields ] = await connection.query(`
+    //         SELECT * FROM Status LIMIT 1
+    //     `)
 
-        res.send({
-            status: results[0]['status'],
-        })
-    } catch (e) {
-        console.error(e.message)
-        res.send({
-            status: 'Database error...',
-        })
-    }
+    //     res.send({
+    //         status: results[0]['status'],
+    //     })
+    // } catch (e) {
+    //     console.error(e.message)
+    //     res.send({
+    //         status: 'Database error...',
+    //     })
+    // }
+
+    res.send({
+        status: 'Hello world!',
+    })
 })
 
 app.listen(process.env['PORT'] ?? 8081)
