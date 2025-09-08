8e5ff3fe9d29526c6302f58359b0466517637e01
Zach Preston <zachattack6089@gmail.com>
2025-09-07 20:54:25 -0700

    Reverted URL change.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,8 +4,10 @@
     const server_status = ref('...')
 
     async function get_server_status() {
-        const api_url = import.meta.env['VITE_BACKEND_URL']
-        const status_url = `${api_url}/status`
+        const host = import.meta.env['VITE_BACKEND_HURL']
+        const port = import.meta.env['VITE_BACKEND_PORT']
+
+        const status_url = `${host}:${port}/status`
         try {
             const response = await fetch(status_url)
             const result = await response.json()
