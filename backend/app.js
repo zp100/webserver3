c36851cb6bc2434c0982a258b2c3de5d6ce27237
Zach Preston <zachattack6089@gmail.com>
2025-09-07 21:15:55 -0700

    Re-enabled database access.

--- backend/app.js
+++ backend/app.js
@@ -6,31 +6,27 @@ const app = express()
 app.use(cors())
 
 app.get('/status', async (req, res) => {
-    // try {
-    //     const connection = await mysql.createConnection({
-    //         host: process.env['DB_HOST'],
-    //         user: process.env['DB_USER'],
-    //         password: process.env['DB_PASSWORD'],
-    //         database: process.env['DB_DATABASE'],
-    //     });
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env['DB_HOST'],
+            user: process.env['DB_USER'],
+            password: process.env['DB_PASSWORD'],
+            database: process.env['DB_DATABASE'],
+        });
 
-    //     const [ results, fields ] = await connection.query(`
-    //         SELECT * FROM Status LIMIT 1
-    //     `)
+        const [ results, fields ] = await connection.query(`
+            SELECT * FROM Status LIMIT 1
+        `)
 
-    //     res.send({
-    //         status: results[0]['status'],
-    //     })
-    // } catch (e) {
-    //     console.error(e.message)
-    //     res.send({
-    //         status: 'Database error...',
-    //     })
-    // }
-
-    res.send({
-        status: 'Hello world!',
-    })
+        res.send({
+            status: results[0]['status'],
+        })
+    } catch (e) {
+        console.error(e.message)
+        res.send({
+            status: 'Database error...',
+        })
+    }
 })
 
 app.listen(process.env['PORT'] ?? 8081)
